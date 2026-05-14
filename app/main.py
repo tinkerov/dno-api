7fceb793720230ec1f424ed960ef5e44eb28f95c
tinkerov <daun123@example.com>
2026-05-14 14:40:16 +0300

    implement user registration and login with argon2 and jwt

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import items
+from .routers import auth
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="DNO")
 
 app.include_router(items.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
---
+++ app/routers/auth.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from jose import jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from .. import models, schemas, database
+
+# Конфиги для JWT
+SECRET_KEY = "SUPER_EBUCHII_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Настройка работы с паролями (хеширование)
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+router = APIRouter(tags=['Authentication'])
+
+# Вспомогательные функции
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# Эндпоинт для регистрации
+@router.post('/register', response_model=schemas.UserResponse)
+def register(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+
+    user_exists = db.query(models.User).filter(models.User.email == user.email).first()
+    if user_exists:
+        raise HTTPException(status_code=400, detail="Email is already registered")
+    
+    hashed_pwd = hash_password(user.password)
+    new_user = models.User(email=user.email, hashed_password=hashed_pwd)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+# Эндпоинт для логина (выдача токена)
+@router.post("/login", response_model=schemas.Token)
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    # OAuth2PasswordRequestForm ожидает username и password, у нас в качестве username - email
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+    if not user or not verify_password(user_credentials.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+
+    access_token = create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}    
