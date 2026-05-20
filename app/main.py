bf152b430514a75d8028d323925e9ef8f530d9e0
tinkerov <tpoi62008@gmail.com>
2026-05-20 06:34:34 +0300

    frontend and backend were successfully merged

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from . import models
 from .database import engine
@@ -13,6 +14,14 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="DNO", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Подключаем items, auth
 app.include_router(items.router)
